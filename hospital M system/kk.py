8fca8f2b2b31ac566949206169ec413e9c2ab12f
sumaya <sumayo672@gmail.com>
2024-12-22 11:33:50 -0800

    Add files via upload

---
+++ hospital M system/kk.py
@@ -0,0 +1,25 @@
+if Statement
+Executes a block of code if the condition is True.
+
+if condition:
+     code block
+
+
+2. if-else Statement
+Executes one block if the condition is True and another block if the condition is False.
+
+if condition:
+      code block if True
+else:
+      code block if False
+
+
+3. if-elif-else Statement
+Checks multiple conditions in sequence. The first True condition's block is executed.
+
+if condition1:
+    code block if condition1 is True
+elif condition2:
+     code block if condition2 is True
+else:
+     code block if none are True
